ecf297b476b7df3f8130a368d0fde62c77d5b895
AbdulazizkhanKarimbayev <a.k.18072003n.b@gmail.com>
2022-05-06 18:36:07 +0500

    Created user and role

---
+++ src/main/java/com/example/lms_project/user/UserDto.java
@@ -0,0 +1,36 @@
+package com.example.lms_project.user;
+import com.example.lms_project.user.role.Role;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserDto {
+
+    private Integer id;
+    private Role role;
+    @NotNull(message = ("User Role id is mandatory!"))
+    private Integer roleId;
+//    private Certificate certificate;
+//    @NotNull(message = ("Certificate id is mandatory!"))
+//    private Integer certificateId;
+    @NotBlank(message = ("User firstname is mandatory!"))
+    private String firstname;
+    @NotBlank(message = ("User lastname is mandatory!"))
+    private String lastname;
+    @NotBlank(message = ("User username is mandatory!"))
+    private String username;
+    @NotBlank(message = ("User phone is mandatory!"))
+    private String phone;
+    @NotBlank(message = ("User password is mandatory!"))
+    private String password;
+}
---
+++ src/main/java/com/example/lms_project/user/role/RoleDto.java
@@ -0,0 +1,18 @@
+package com.example.lms_project.user.role;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class RoleDto {
+    private Integer id;
+    @NotBlank(message = ("Role name is mandatory!"))
+    private String name;
+}

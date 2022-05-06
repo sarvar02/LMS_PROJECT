7f6005674f0210322216886550021d56d898dcd7
sarvar <sarvartulkunov2002@gmail.com>
2022-05-06 17:29:43 +0500

    Initial commit

---
+++ src/test/java/com/example/lms_project/LmsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lms_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

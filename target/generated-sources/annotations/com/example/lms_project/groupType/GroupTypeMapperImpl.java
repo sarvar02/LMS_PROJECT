d0b4d5e39b07986d3f6fbe0de3f5de7dea580561
sarvar <sarvartulkunov2002@gmail.com>
2022-05-09 11:12:38 +0500

    Group Type, insertable and updatable

---
+++ target/generated-sources/annotations/com/example/lms_project/groupType/GroupTypeMapperImpl.java
@@ -0,0 +1,41 @@
+package com.example.lms_project.groupType;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-05-09T09:23:35+0500",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.15 (Private Build)"
+)
+@Component
+public class GroupTypeMapperImpl implements GroupTypeMapper {
+
+    @Override
+    public GroupType dtoToModel(GroupTypeDto groupTypeDto) {
+        if ( groupTypeDto == null ) {
+            return null;
+        }
+
+        GroupType groupType = new GroupType();
+
+        groupType.setId( groupTypeDto.getId() );
+        groupType.setName( groupTypeDto.getName() );
+
+        return groupType;
+    }
+
+    @Override
+    public GroupTypeDto modelToDto(GroupType groupType) {
+        if ( groupType == null ) {
+            return null;
+        }
+
+        GroupTypeDto groupTypeDto = new GroupTypeDto();
+
+        groupTypeDto.setId( groupType.getId() );
+        groupTypeDto.setName( groupType.getName() );
+
+        return groupTypeDto;
+    }
+}
